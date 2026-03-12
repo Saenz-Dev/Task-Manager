8215d8c82ba95b4f318e30a0533c87307ca139f5
Saenz-Dev <miguel.saenz02@uptc.edu.co>
2026-03-12 09:32:19 -0500

    Creación del proyecto y agregación de componente Login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+
+registerLocaleData(es);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes), provideNzI18n(es_ES)
   ]
 };
